8e4a25902662f79818e9925f2aa842927d5ccc9a
chelseyqc <6721@holbertonstudents.com>
2024-01-08 14:37:22 +0800

    feat: an async routine called wait_n

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""an async routine called wait_n"""
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    takes 2 two args n and max_delay and returns the list of all the delays
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    completed = await asyncio.gather(*tasks)
+    return completed
