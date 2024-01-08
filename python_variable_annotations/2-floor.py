0979193813fcb4f261cc5b6b4b3d198472ce1e68
chelseyqc <6721@holbertonstudents.com>
2024-01-08 09:27:39 +0800

    feat: added a type-annotated function floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor which takes a float n
+"""
+
+
+def floor(n: float) -> int:
+    """takes float argument and returns the floor of the float"""
+    return int(n)
