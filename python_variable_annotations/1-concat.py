eebef8ee728cde7cff117f8db3a2a1509326799f
chelseyqc <6721@holbertonstudents.com>
2024-01-08 09:20:33 +0800

    feat: added a type annotated function concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function concat that takes a string str1 and str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes string arguments and returns a concatenated string"""
+    return str1 + str2
