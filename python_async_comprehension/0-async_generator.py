661760f3cc7636379c059fd83a022d5be4f2fc34
chelseyqc <6721@holbertonstudents.com>
2024-01-09 15:22:41 +0800

    feat: added a coroutine called async_generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""a coroutine that loops 10 times"""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    loops 10 times and each time it will asynchronously wait for 1 sec
+    then yield a random number between 0 and 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
