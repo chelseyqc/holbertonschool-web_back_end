f19a9972293a6dc23a1b8ed1ea3a4be748042515
chelseyqc <6721@holbertonstudents.com>
2024-01-09 15:39:08 +0800

    fix: changed return

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """a coroutine called async_comprehension that takes no arguments"""
 import asyncio
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     collects 10 random numbers using async comprehensing over async_generator
     and returns the 10 random numbers
