b29583d58ac5c557fb8d8470ec911be61dd346f2
chelseyqc <6721@holbertonstudents.com>
2024-01-10 14:48:36 +0800

    docs: documented function

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,5 +4,6 @@ from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
+    """Lists all documents in a collection and returns a list"""
     documents = mongo_collection.find()
     return list(documents)
