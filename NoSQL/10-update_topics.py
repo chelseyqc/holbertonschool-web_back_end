cf34301d5d5057edf99b00733b7dfe016d909532
chelseyqc <6721@holbertonstudents.com>
2024-01-10 14:59:17 +0800

    docs: documented function

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 
 
 def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a document based on name"""
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
