0ebe25050abfe09b7d5b8f1a1d0934d22e66b872
chelseyqc <6721@holbertonstudents.com>
2024-01-26 08:50:18 +0800

    fix: eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,14 @@
 import signUpUser from './4-user-promise';
 import { uploadPhoto } from './utils';
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName)
-    .then(value => ({ status: 'fulfilled', value }))
-    .catch(error => ({ status: 'rejected', value: `${error.message}`}));
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: `${error.message}` }));
 
   const photoPromise = uploadPhoto(fileName)
-    .then(value => ({ status: 'fulfilled', value }))
-    .catch(error => ({ status: 'rejected', value: `${error.message}`}));
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: `${error.message}` }));
 
   return Promise.all([userPromise, photoPromise]);
 }
