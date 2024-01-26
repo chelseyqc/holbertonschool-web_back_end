e72698f1b20fee3770ea4c5aeb1c9f427adb068c
chelseyqc <6721@holbertonstudents.com>
2024-01-26 08:45:08 +0800

    fix: changed console log output

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,11 @@
-import { uploadPhoto, createUser } from "./utils"
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-  .then(([photoResponse, userResponse]) => {
-    console.log(`${userResponse.firstName} ${userResponse.lastName}`);
-  })
-  .catch(() => {
-    console.log("Signup system offline");
-  })
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
